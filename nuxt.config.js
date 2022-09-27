c3c1c1c013a4a5cc3c5c176eb3ca297be2db47bf
tokukait <aria.kqja@gmail.com>
2022-09-27 17:53:55 +0900

    modify nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,5 +59,19 @@ export default {
         autoprefixer: {},
       },
     },
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    }
   }
 }
